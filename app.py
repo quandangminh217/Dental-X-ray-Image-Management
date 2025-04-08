aef8c106a3a24c83b9b2871f01d7629ab0b4d348
quandangminh217 <quandangminh217@gmail.com>
2025-04-08 11:59:05 +0700

    Update app.py

--- app.py
+++ app.py
@@ -22,4 +22,5 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Lấy cổng do Render cung cấp
+    app.run(host='0.0.0.0', port=port)
